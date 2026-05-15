90bf223f3071b19ca146ac4b7349fc8df2446bbf
bhaktipg <patbhakti04@gmail.com>
2026-05-14 20:02:03 -0400

    Update test_emotion_detection.py

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -4,19 +4,19 @@ import unittest
 class TestEmotionDetector(unittest.TestCase):
     def test_emotion_detector(self):
         result1= emotion_detector("I am glad this happened")
-        self.assertEqual(result1["domninant_emotion"],'joy')
+        self.assertEqual(result1["dominant_emotion"],'joy')
 
         result2=emotion_detector("I am really mad about this")
-        self.assertEqual(result2["domninant_emotion"],'anger')
+        self.assertEqual(result2["dominant_emotion"],'anger')
 
         result3=emotion_detector("I feel disgusted just hearing about this")
-        self.assertEqual(result3["domninant_emotion"],'disgust')
+        self.assertEqual(result3["dominant_emotion"],'disgust')
 
         result4=emotion_detector("I am so sad about this")
-        self.assertEqual(result4["domninant_emotion"],'sadness')
+        self.assertEqual(result4["dominant_emotion"],'sadness')
 
         result5=emotion_detector("I am really afraid that this will happen")
-        self.assertEqual(result5["domninant_emotion"],'fear')
+        self.assertEqual(result5["dominant_emotion"],'fear')
 if __name__ == '__main__':
     unittest.main()
 
